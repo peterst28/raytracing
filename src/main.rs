4876d144eb3007a99e73959e293e07efbfea320f
Peter Stern <peterst28@gmail.com>
2023-08-15 15:56:14 +0200

    Added normals to sphere

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,26 @@ use crate::ray::Ray;
 mod vec3;
 mod ray;
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - *center;
     let a = Vec3::dot(&r.direction(), &r.direction());
     let b = 2.0 * Vec3::dot(&oc, &r.direction());
     let c = Vec3::dot(&oc, &oc) - radius * radius;
     let discriminant = b*b - 4.0*a*c;
-    return discriminant >= 0.0;
+
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt() ) / (2.0*a);
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
 
-    if hit_sphere(&Point3::new(0.0,0.0,-1.0), 0.5, &r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point3::new(0.0,0.0,-1.0), 0.5, &r);
+    if t > 0.0 {
+        let N = Vec3::unit_vector( &(r.at(t) - Vec3::new(0.0,0.0,-1.0)));
+        return 0.5 * Color::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
     }
 
     let unit_direction = Vec3::unit_vector(&r.direction());
